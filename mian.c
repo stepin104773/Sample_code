43d3f98136753c9d8b60404599f44ea9becc5dcd
stepin104773 <69396587+stepin104773@users.noreply.github.com>
2020-09-27 17:09:11 +0530

    Create mian.c

---
+++ mian.c
@@ -0,0 +1,15 @@
+#include "Checkisogram.h"
+int main() {
+    char s[100],ls[100];
+    int c=0,i;
+    scanf("%s",s);
+    for(i=0;i<strlen(s);i++)
+    {
+        ls[i]=tolower(s[i]);
+    }
+    c=isogram(ls);
+    if(c==0)
+    printf("NO");
+    else
+    printf("YES");
+}
