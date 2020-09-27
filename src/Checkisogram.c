037bdd2c0568151d5986716c2b919b3c34670b8f
stepin104773 <69396587+stepin104773@users.noreply.github.com>
2020-09-27 17:01:58 +0530

    Create Checkisogram.c

---
+++ src/Checkisogram.c
@@ -0,0 +1,16 @@
+#include "Checkisogram.h>
+int isogram(char ls[])
+{
+    int i=0,j=0;
+    for(i=0;i<strlen(ls);i++)
+    {
+        for(j=i+1;j<=strlen(ls);j++)
+        {
+            if(ls[i]==ls[j])
+            return 0;
+        }
+
+        //1=true 0=false
+    }
+return 1;
+}
